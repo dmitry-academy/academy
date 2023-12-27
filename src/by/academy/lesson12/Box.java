e8d8c2d82f95e5e9da6eae27f3e46227c0a13b3d
Dzmitry Shchepin <Dzmitry.Shchepin@gmail.com>
2023-12-27 20:03:33 +0300

    lesson 12

---
+++ src/by/academy/lesson12/Box.java
@@ -0,0 +1,41 @@
+package by.academy.lesson12;
+
+public class Box<T> {
+
+    private T[] items;
+    private int current = 0;
+
+    public Box() {
+        super();
+        this.items = (T[]) new Object[10];
+    }
+
+    public Box(T[] items) {
+        this.items = items;
+    }
+
+    public void add(T item) {
+        if (items == null) {
+            this.items = (T[]) new Object[10];
+        }
+
+        if (items.length <= current) {
+            grow();
+        }
+
+        items[current++] = item;
+    }
+
+    private void grow() {
+        T[] temp = (T[]) new Object[items.length * 2 + 1];
+        System.arraycopy(items, 0, temp, 0, items.length);
+        items = temp;
+    }
+
+
+    public void print() {
+        for (int i = 0; i < current; i++) {
+            System.out.println(items[i]);
+        }
+    }
+}
